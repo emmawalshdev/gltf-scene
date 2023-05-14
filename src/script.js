35f9fb0521e0a19448f201e2c306219753304496
Emma Walsh <emmaharte33@outlook.com>
2023-05-14 20:24:56 +0100

    Add lights and gui controls for lights

--- src/script.js
+++ src/script.js
@@ -14,6 +14,11 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+
+// light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(0.25,3,-2.25);
+scene.add(directionalLight);
 /**
  * Test sphere
  */
@@ -23,6 +28,15 @@ const testSphere = new THREE.Mesh(
 )
 scene.add(testSphere)
 
+// gui tweaks
+gui
+  .add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity');
+gui
+  .add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('lightX');
+gui
+  .add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('lightY');
+gui
+  .add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('lightZ');
 /**
  * Sizes
  */
