99c2cf48544161c190c1495f086fd5f17a734d8e
Emma Walsh <emmaharte33@outlook.com>
2023-05-14 21:13:06 +0100

    traverse scene chilren and upfate envmap intensity via gui

--- src/script.js
+++ src/script.js
@@ -13,12 +13,23 @@ const cubeTextureLoader = new THREE.CubeTextureLoader();
  */
 // Debug
 const gui = new dat.GUI()
+const debugObject = {};
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+// update all materials
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial){
+            child.material.envMap = environmentMap;
+            child.material.envMapIntensity = debugObject.envMapIntensity; // update with debug value
+        }
+    });
+}
 
 // Environmental map
 const environmentMap = cubeTextureLoader.load([
@@ -30,8 +41,12 @@ const environmentMap = cubeTextureLoader.load([
     '/textures/environmentMaps/0/nz.jpg',
 ]);
 
-console.log(environmentMap);
 scene.background = environmentMap;
+
+debugObject.envMapIntensity = 5; // set envmapintensity of emty objec
+gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001) // add tweak for above
+.onChange(updateAllMaterials); // update value on change
+
 
 // models
 gltfLoader.load(
@@ -43,6 +58,8 @@ gltfLoader.load(
         gltf.scene.rotation.y = Math.PI * 0.5;
 
         gui.add(gltf.scene.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('rotation');
+        updateAllMaterials();
+
     }
 )
 
