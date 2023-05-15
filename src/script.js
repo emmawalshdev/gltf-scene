4984f793ac6b7001c4215c0d9dde76170ed2be85
Emma Walsh <emmaharte33@outlook.com>
2023-05-15 22:02:34 +0100

    Add tone mapping

--- src/script.js
+++ src/script.js
@@ -126,6 +126,18 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.physicallyCorrectLights = true;
 renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping
+});
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001);
 /**
  * Animate
  */
